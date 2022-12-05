98719617bdde35e90d8c0ab7c0d3f97ac5151ba9
Emanuel <emanuel.fonseca@ua.pt>
2022-12-05 17:23:16 +0000

    comit com alterações

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def face_confidence(face_distance, face_match_threshold=0.6):
         value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
         return str(round(value, 2)) + '%'
 
-
+#tfrfhvtfhg
 
 
 class FaceRecognition:
